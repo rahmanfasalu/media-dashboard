eff2ed209a769cca5929b381371812f57180fad6
Fasalu Rahman <fasalukm@gmail.com>
2020-07-14 21:49:15 +0200

    Deploy

---
+++ precache-manifest.1bea847a9fe4a6ec339006fb3f86faa7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "329c76b71ac0ce85dbed31b3aec02335",
+    "url": "/media-dashboard/index.html"
+  },
+  {
+    "revision": "ee097c8ade9eee803065",
+    "url": "/media-dashboard/static/css/2.cd7aafbe.chunk.css"
+  },
+  {
+    "revision": "ee097c8ade9eee803065",
+    "url": "/media-dashboard/static/js/2.62568bd0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/media-dashboard/static/js/2.62568bd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9362969d5c1a159c0b0",
+    "url": "/media-dashboard/static/js/main.b8d610aa.chunk.js"
+  },
+  {
+    "revision": "4e2ab945c85f0b24545d",
+    "url": "/media-dashboard/static/js/runtime-main.0c8c3dd7.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/media-dashboard/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/media-dashboard/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/media-dashboard/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/media-dashboard/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/media-dashboard/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "4682c75a573272110fd11da11784d30c",
+    "url": "/media-dashboard/static/media/logo.4682c75a.jpeg"
+  }
+]);
